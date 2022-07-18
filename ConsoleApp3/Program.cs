41e5583e7506cfeddeeb84ceae01c76189c6bffd
bpeixoto96 <peixotobruno17@gmail.com>
2022-07-18 16:07:35 -0300

    C# Calculadora da Mallu

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com o valor que voce quer saber");
+            int xvalor = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= 10; i++)
+                Console.WriteLine("a tabuada é " + xvalor + " X " + i + " = " + (xvalor * i));
+
+        }
+    }
+}
